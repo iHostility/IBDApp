815b42513fa13dead7161c99d3164d481db5f803
iHostility <ihostility@gmail.com>
2019-03-10 23:41:51 +0300

    Minor bugfix

--- src/main/java/consoleflood/EstablishConnection.java
+++ src/main/java/consoleflood/EstablishConnection.java
@@ -1,26 +1,32 @@
 package consoleflood;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 class EstablishConnection {
-    private final String driverName = "com.mysql.cj.jdbc.Driver";
-    private final String connectionString = "jdbc:mysql://localhost:3306/ibdapp?useSSL=false";
-    private final String login = "service";
-    private final String password = "1";
 
     void run() {
+        final String url = "jdbc:mysql://localhost:3306/ibdapp?useSSL=false";
+        final String login = "service";
+        final String password = "1";
+
+        Driver driver;
         try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
+            driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+
+        } catch (SQLException e) {
             System.out.println("Can't get class. No driver found");
             e.printStackTrace();
             return;
         }
 
         try {
-            Main.connection = DriverManager.getConnection(connectionString, login, password);
+            Main.connection = DriverManager.getConnection(url, login, password);
             System.out.println("Connection established!");
         } catch (SQLException e) {
             System.out.println("Can't get connection. Incorrect URL");
