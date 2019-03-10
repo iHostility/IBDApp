11e017c8af70e63654f091bfd6e22839d7f4800f
iHostility <ihostility@gmail.com>
2019-03-10 14:40:55 +0300

    Add sql statements

--- src/main/java/consoleflood/Main.java
+++ src/main/java/consoleflood/Main.java
@@ -6,23 +6,20 @@ import java.sql.Connection;
  * General console flood for work activity imitation
  * made by iHostility
  * Voloshin Denis
- *
  */
 
-public class Main
-{
+public class Main {
     static Connection connection = null;
 
-    public static void main( String[] args )
-    {
-        EstablishConnection startConnection = new EstablishConnection();
-        startConnection.run();
+    public static void main(String[] args) {
+        new EstablishConnection().run();
 
         //TODO add to do some stuff here
         System.out.println("RRRRREEEEEEEEE");
+        new Output().run();
 
-        ShutdownConnection closeConnection = new ShutdownConnection();
-        closeConnection.run();
+        new ShutdownConnection().run();
+
 
     }
 }
--- src/main/java/consoleflood/Output.java
+++ src/main/java/consoleflood/Output.java
@@ -1,5 +1,21 @@
 package consoleflood;
 
-public class Output {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+class Output {
+    void run() {
+        try (Connection exec = Main.connection; Statement statement = exec.createStatement()) {
+           statement.execute("select line from ibdapp.dictionary where phrase_id = '1';");
+           //
+           //
+           //
+           //
+           //
+           //
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
