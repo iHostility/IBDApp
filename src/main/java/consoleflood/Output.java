4aa529d233a7811d894231f3406abfac43abc16a
iHostility <ihostility@gmail.com>
2019-03-10 23:18:37 +0300

    Minor bugfix

--- src/main/java/consoleflood/Output.java
+++ src/main/java/consoleflood/Output.java
@@ -9,17 +9,15 @@ class Output {
             String query = "select * from ibdapp.dictionary";
             int a = 10;
             int b = 5000;
-            int delay = a + (int) (Math.random() * b);
 
             PreparedStatement prepStat = exec.prepareStatement(query);
             ResultSet resSet = prepStat.executeQuery();
-            int i = 20;
+
             while (resSet.next()) {
+                int delay = a + (int) (Math.random() * b);
                 System.out.println(resSet.getString("line"));
                 Thread.sleep(delay);
-                i--;
-                if (i == 0) break;
-            }
+             }
 
         } catch (SQLException ex) {
             ex.printStackTrace();
